e5ce9439e2eccd73b111d78f9131ff4bfe3a3a4c
alextrifan1 <alexandrutrifan17@gmail.com>
2026-06-06 23:15:23 +0300

    feat(frontend): initialize react app with routing and state
    - Scaffold Vite React TS app and Dockerize
    - Configure Tailwind CSS v4 for UI styling
    - Implement React Router with Dashboard and Login placeholders
    - Setup Redux Toolkit with global Auth slice

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+    return (
+        <div className="min-h-screen flex intems-container justify-center bg-gray-50 text-gray-900 font-sans">
+            <h1 className="text-2xl">Login</h1>
+        </div>
+    )
+}
